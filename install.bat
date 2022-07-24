09fec8ac26fe8462d2ec3343debe8b15a37f24d2
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2022-07-24 20:25:13 +0000

    Delete install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-echo NOTE: Make sure you have NodeJS and Visual Studio with all C++ prequisites installed. Otherwise this won't work
-
-call npm install .
-call npm install -g pkg
-call npm install node-gyp
-call npm install win-dpapi
-call npm rebuild
