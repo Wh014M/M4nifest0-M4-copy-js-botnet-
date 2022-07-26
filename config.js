bfe75b1c9ab96adb13f0ecbb9431ef70d07c9a5f
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2022-07-26 08:04:36 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client) => {
-    return {
-        url: "https://discordapp.com/api/webhooks/1000859793784832011/2ocjHFMEgrtfwIKectQtJhYFe-OuYZvfSL_scIj5FzBJ00jI524v8wb_E1GZZKjrdfwC",
-    }
-}
